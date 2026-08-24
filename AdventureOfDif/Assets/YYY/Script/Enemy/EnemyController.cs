7853aee60bea0174fad844178df55687c4228106
FT Girl <hakuryu999hdbl@gmail.com>
2026-08-24 22:17:58 +0900

    260824_All_Enemy_Catch

--- AdventureOfDif/Assets/YYY/Script/Enemy/EnemyController.cs
+++ AdventureOfDif/Assets/YYY/Script/Enemy/EnemyController.cs
@@ -222,7 +222,7 @@ public class EnemyController : MonoBehaviour
                 }
                 else
                 {
-                    if (this is Enemy_3)
+                    if (this is Enemy_4)
                     {
                         anim.SetTrigger("catch");
                     }
